91df65005916801b077d4f230f5e61925e8b0ba3
mcV3rs <mcVers.pl@gmail.com>
2022-03-31 16:32:15 +0200

    Pierwsza testowa wersja modułu JSONa

---
+++ JSON/JSON/JSON/Models/quiz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Models
+{
+    internal class quiz
+    {
+        public List<Quiz> Quiz { get; set; }
+    }
+
+    public class Quiz
+    {
+        public int ID { get; set; }
+
+        public string Question { get; set; }
+
+        public string Answer_A { get; set; }
+
+        public string Answer_B { get; set; }
+
+        public string Answer_C { get; set; }
+
+        public string Answer_D { get; set; }
+
+        public int Correct { get; set; }
+    }
+}
