d4871b4047d9b20c06245ca94fc7deb932c1c7a7
adamek117 <72439801+adamek117@users.noreply.github.com>
2022-03-31 21:57:04 +0200

    Add files via upload

---
+++ Adam/Quiz/Quiz/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Quiz.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            deserialized();
+        }
+        public void deserialized()
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText(@"pytania.json");
+
+                 JsonConvert.DeserializeObject<List<Quize>>(json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
